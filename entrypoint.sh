dbe57cf10891150414f774eef0a59424400366d2
harun <harun@haruns-Air.attlocal.net>
2022-03-04 21:33:27 -0500

    deneme

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo $(ls /)
+echo $(ls /src)
 echo $(/github)
 echo $(/github/workspace)
 echo "::set-output name=pdf-path::name-value"
-cat /github/workspace/README.md
+cat /github/workspace/src/a.py
+
+python3 /src/test_a.py
